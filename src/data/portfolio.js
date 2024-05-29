8c6128a82929518ed92355afabf6f9fb1df00de3
Aviroop Pal <avirooppal42@gmail.com>
2024-05-29 13:35:38 +0530

    29-05-2024 Pictures Added

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
        title: 'Reinforcement Learning 🐍 Snake Idea ',
-       imgUrl: './public/Reinforcement Learning.jpg',
+       imgUrl: 'https://github.com/avirooppal/Aviroop-Portfolio/blob/master/public/Reinforcement%20Learning.jpg?raw=true',
        stack: ['Python', 'Pygame', 'PyTorch'],
        link: 'https://github.com/avirooppal/RL-Learning-Snake.git',
     },
     {
        title: "👀's for the AI made using Gemeni Vision",
-       imgUrl: './public/Eyes.jpg',
+       imgUrl: 'https://github.com/avirooppal/Aviroop-Portfolio/blob/master/public/Eyes.jpg?raw=true',
        stack: ['Python', 'Gemini', 'OpenCV'],
        link: 'https://github.com/avirooppal/Gemini-Vision-.git',
     },
     {
        title: 'Chat with Multiple Custom PDF Documents📃',
-       imgUrl: './public/Chat.jpg',
+       imgUrl: 'https://github.com/avirooppal/Aviroop-Portfolio/blob/master/public/Chat.jpg?raw=true',
        stack: ['Python', 'Streamlit', 'Langchain'],
        link: 'https://github.com/avirooppal/Chat-with-Multiple-PDF-s---Langchain-And-Gemini-.git',
     },
